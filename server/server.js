a529bb21606edef3df3014ab80755e70f13440d5
segullahprojects0-cloud <segullahprojects0@gmail.com>
2026-02-17 05:46:09 -0800

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://segullahprojects0-cloud.github.io",
+    "https://segullahprojects.co.za"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
